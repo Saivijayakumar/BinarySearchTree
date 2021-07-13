eb5edf0f0a0aeb49419b73857de0a706ede030ca
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-13 16:04:49 +0530

    [SaiVijayaKumar] Creating BST

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,6 +11,11 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t----------->Welcome to Binary Search Tree<----------------");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            Console.WriteLine("InOrder Traversal For Given Is BST ");
+            binarySearch.Display();
             Console.ReadLine();
         }
     }
