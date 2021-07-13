0b40cbb061a7900973310168fa6868e6adb85165
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-13 15:15:40 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        /// <summary>
+        /// ----------------->Welcome to Binary Search Tree<--------------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----------->Welcome to Binary Search Tree<----------------");
+            Console.ReadLine();
+        }
+    }
+}
